6901ba15defa975fd4adb017df20f23cf03c68fb
Artem Novichkov <mail@artemnovichkov.com>
2025-07-01 21:05:49 +0500

    Add toolbar for text editor

--- iOS-26-by-Examples/Views/RichTextEditor.swift
+++ iOS-26-by-Examples/Views/RichTextEditor.swift
@@ -5,14 +5,18 @@
 import SwiftUI
 
 struct RichTextEditor: View {
+    @Environment(\.fontResolutionContext) private var fontResolutionContext
     @State private var text: AttributedString = ""
+    @State private var selection = AttributedTextSelection()
+    @FocusState private var isFocused: Bool
 
     var body: some View {
-        TextEditor(text: $text)
+        TextEditor(text: $text, selection: $selection)
+            .focused($isFocused)
             .padding(.horizontal)
             .onAppear {
                 var text = AttributedString(
-                  "Hello 👋🏻! Who's ready to get "
+                    "Hello 👋🏻! Who's ready to get "
                 )
 
                 var cooking = AttributedString("cooking")
@@ -23,8 +27,34 @@ struct RichTextEditor: View {
 
                 text.font = .largeTitle
                 self.text = text
+                isFocused = true
             }
             .navigationTitle("Rich text editor")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    HStack {
+                        Button("Bold", systemImage: "bold") {
+                            text.transformAttributes(in: &selection) { container in
+                                let currentFont = container.font ?? .default
+                                let resolved = currentFont.resolve(in: fontResolutionContext)
+                                container.font = currentFont.bold(!resolved.isBold)
+                            }
+                        }
+                        .frame(width: 32, height: 32)
+
+                        Button("Italic", systemImage: "italic") {
+                            text.transformAttributes(in: &selection) { container in
+                                let currentFont = container.font ?? .default
+                                let resolved = currentFont.resolve(in: fontResolutionContext)
+                                container.font = currentFont.italic(!resolved.isItalic)
+                            }
+                        }
+                        .frame(width: 32, height: 32)
+
+                        Spacer()
+                    }
+                }
+            }
     }
 }
 
